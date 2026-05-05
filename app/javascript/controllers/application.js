6e16e1ff1d90ddc025ad6905b70633bee6dce7a8
smnvkh <s.masha@me.com>
2026-05-05 14:06:25 +0500

    Menu scroll

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,23 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from '@hotwired/stimulus'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
+
+document.addEventListener('turbo:load', () => {
+  const menu = document.querySelector('.S_MenuBar')
+
+  if (!menu) return
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+      menu.classList.add('scrolled')
+    } else {
+      menu.classList.remove('scrolled')
+    }
+  })
+})
