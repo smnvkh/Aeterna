7d47554e4311746ec6888a46309152127c7fcb91
smnvkh <s.masha@me.com>
2026-06-25 01:32:42 +0500

    edit posts pages and mobile profile

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,6 +2,8 @@ class Memory < ApplicationRecord
   belongs_to :family
   belongs_to :family_member
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :collection_memories, dependent: :destroy
+  has_many :collections, through: :collection_memories
 
   delegate :user, to: :family_member, allow_nil: true
 
